937b4825cc425ea8e488aaca2a5819d1e4f3ebbc
Mahendra <masterkingmahendra@gmail.com>
2017-09-16 18:50:19 +0530

    Added test for service class

--- assignment/src/app/services/mock-data.service.spec.ts
+++ assignment/src/app/services/mock-data.service.spec.ts
@@ -1,19 +1,44 @@
-import { TestBed, inject } from '@angular/core/testing';
-// import { Http } from '@angular/http';
-import { HttpModule } from '@angular/http';
-
+import * as console from 'console';
+import { TestBed, inject, async } from '@angular/core/testing';
+import { HttpModule, Http, Response, ResponseOptions, XHRBackend } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 import { MockDataService } from './mock-data.service';
 
 describe('MockDataService', () => {
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpModule], 
-      providers: [MockDataService]
+      providers: [
+        MockDataService,
+        { provide: XHRBackend, useClass: MockBackend }
+      ]
     });
-  });
+  }));
 
-  it('should be created', inject([MockDataService], (service: MockDataService) => {
+  it('should be created', inject([MockDataService], (service) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('getBooking()', ()=> {
+   
+    it('should return an obsevable with mock data', inject([MockDataService, XHRBackend], (service, mockBackend) => {
+      const mockResponse = {
+          bookingCode:'ABCDE',
+          familyName: 'FOO'
+      };
+
+      mockBackend.connections.subscribe((connection) => {
+        connection.mockRespond(new Response(new ResponseOptions({
+          body: JSON.stringify(mockResponse)
+        })));
+      });
+
+      service.getBooking('fakeUrl').subscribe( data => {
+        expect(data.bookingCode).toEqual('ABCDE');
+        expect(data.familyName).toEqual('FOO');
+      })
+
+    }))
+  })
 });
